a5578b0a7d85acec2f107be7a3a8f94641437fc9
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-09-05 22:53:28 -0400

    feature: funcion filtrar busqueda por texto

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-const botonEliminar = document.querySelector("#limpiar")
-const busqueda = document.querySelector("#busqueda")
-const listaCheckBox = document.querySelectorAll("input[type ='checkbox'][name='categoria']")
+const botonLimpiar = document.querySelector("#limpiar")
+const filtroNombre = document.querySelector("#busqueda")
+const checkBoxes = document.querySelectorAll("input[type ='checkbox']")
+
+const tarjetas = document.getElementsByClassName('card-producto')
+
+
+
+// funcion filtrar busqueda por textbox
+filtroNombre.oninput = () => {
+
+    for (let tarjeta of tarjetas) {
+
+        const titulo = tarjeta.dataset.nombre;
+        const busqueda = filtroNombre.value;
+
+        if (titulo.includes(busqueda)) {
+
+            tarjeta.classList.remove('hidden');
+        } else {
+
+            tarjeta.classList.add('hidden');
+        }
+    }
+};
 
 
 
 //funcion limpiar 
-botonEliminar.onclick = () => {
-    busqueda.value = ""
+botonLimpiar.onclick = () => {
+    filtroNombre.value = ""
 
-    for (let checkBox of listaCheckBox) {
+    for (let checkBox of checkBoxes) {
         if (checkBox.checked) {
             checkBox.checked = false;
         }
     }
 
-}
-
-
+};
